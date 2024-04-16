4668ff6bb15121f7c5401a13bb4265ee60d86ecc
AlessandrodeAlmeida2 <agostini55@gmail.com>
2024-04-16 15:00:40 -0300

    UpdateVeiw atualizada para todas as opções da tebela

--- my-app/src/components/UpdateView.vue
+++ my-app/src/components/UpdateView.vue
@@ -18,7 +18,7 @@
   const updateItem = async () => {
     const { data, error } = await supabase
       .from('tabela1')
-      .update({ name: item.value.name, description: item.value.description })
+      .update({ name: item.value.name, description: item.value.description, recompensa: item.value.recompensa, genero: item.value.genero, specie: item.value.specie, situation: item.value.situation })
       .eq('id', itemId)
 
     if (error) {
@@ -31,11 +31,24 @@
 
 <template>
   <div v-if="item">
+
+    <label for="situation">Situação</label>
+    <input id="situation" v-model="item.situation" />
+
     <label for="name">Nome</label>
     <input id="name" v-model="item.name" />
 
     <label for="description">Descrição</label>
     <input id="description" v-model="item.description" />
+
+    <label for="genero">Gênero</label>
+    <input id="genero" v-model="item.genero" />
+
+    <label for="specie">Espécie</label>
+    <input id="specie" v-model="item.specie" />
+    
+    <label for="recompensa">recompensa</label>
+    <input id="recompensa" v-model="item.recompensa" />
 
     <button @click="updateItem">Atualizar</button>
   </div>
