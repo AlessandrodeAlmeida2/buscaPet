f8752db8128bcdbf13027a44d155d968a9c01e3a
˜Takeyama77˜ <pablotakeyama@outlook.com>
2024-10-01 21:40:31 -0300

    botao sem impulsionar

--- my-app/src/components/PaymentView.vue
+++ my-app/src/components/PaymentView.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="showcase">
-    <div class="nes-container with-title">
-      <h3>Impulsionar publicação</h3>
+    <div class="showcase with-title">
+      <h2 class="title">Impulsionar publicação</h2>
       <div class="img">
         <img src="" alt="" srcset="" />
       </div>
@@ -10,19 +10,31 @@
 
   <div class="nes-field mt">
     <stripe-pricing-table :pricing-table-id="pricingTableId"
-    :publishable-key="publishableKey">
+                          :publishable-key="publishableKey">
     </stripe-pricing-table>
+    
+
+    <button @click="goToRead" class="prosseguir__botao">
+      Prosseguir sem impulsionar
+    </button>
   </div>
 </template>
 
 <script>
 import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router'; 
 
 export default {
   name: 'PaymentView',
   setup() {
     const publishableKey = ref(import.meta.env.VITE_STRIPE_KEY);
     const pricingTableId = ref('prctbl_1Q0S4JKaU1Vv323oYYb3E03u');
+    
+    const router = useRouter(); 
+
+    const goToRead = () => {
+      router.push('/read'); 
+    };
 
     onMounted(() => {
       const script = document.createElement('script');
@@ -33,8 +45,31 @@ export default {
 
     return {
       publishableKey,
-      pricingTableId
+      pricingTableId,
+      goToRead 
     };
   }
 };
 </script>
+
+<style>
+.title {
+  text-align: center;
+  font-size: 2.0em;
+}
+
+.prosseguir__botao {
+  display: block; 
+  margin: 16px auto; 
+  padding: 12px 24px; 
+  background-color: #2eb921; 
+  color: white; 
+  border: none; 
+  border-radius: 5px; 
+  cursor: pointer; 
+  text-align: center; 
+  width: 200px; 
+  height: 70px; 
+}
+
+</style>
